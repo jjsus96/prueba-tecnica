1603283590ada64d2a47d6771009f10f46294c2a
jjsus96 <jesus.sayago.mey.1996@gmail.com>
2022-06-07 16:26:09 +0200

    Hasta Creación de rutas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const [filterText, setFilterText] = useState('');
+
+  const handleChangeFilterParameter = (e) => setFilterText(e.target.value)
+
+  useEffect(() => {
+    console.log(filterText)
+  }, [filterText]);
+
+  useEffect(() => {
+    showUsers();
+  }, []);
+
+  const endpoint = 'https://jsonplaceholder.typicode.com'
+
+  const showUsers = async () => {
+    const response = await fetch(`${endpoint}/users`);
+    const data = await response.json();
+    setUsers(data);
+    console.log(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="card-grid-user">
+      <input
+        id="search"
+        type="text"
+        placeholder="Filter By Name"
+        aria-label="Search Input"
+        value={filterText}
+        onChange={handleChangeFilterParameter}
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Usuario</th>
+            <th>Email</th>
+            <th>Teléfono</th>
+            <th>Editar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users
+          .filter ((user) => {
+            if(user.name.includes(filterText)) return true
+            else if(user.username.includes(filterText)) return true
+            else if(user.email.includes(filterText)) return true
+            else if(user.phone.includes(filterText)) return true
+            else return false
+            }
+            )
+          .map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+              <td>{user.phone}</td>
+              <td><button><a href={"/users/"+user.id+"/editor"}>Editar</a></button></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  );
+  )
 }
 
 export default App;
